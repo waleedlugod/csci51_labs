5a7b0ffa4a69cf2cf88b9c8e51f72ea571d362dd
Waleed Lugod <lugodwaleed@gmail.com>
2025-01-27 10:37:40 +0800

    do agent thing

--- Lab2_Lugod_Regina/Lab2_LugodRegina_code.cpp
+++ Lab2_Lugod_Regina/Lab2_LugodRegina_code.cpp
@@ -2,3 +2,22 @@
 // I have not discussed the c++ code in my program with anyone other than my instructor or the teaching assistants assigned to this course.
 // I have not used c++ code obtained from another student, or any other unauthorized source, whether modified or unmodified.
 // If any c++ code or documentation used in my program was obtained from another source, it has been clearly noted with citations in the comments of my program.
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+
+        // get the rest of the string in order to start the next line
+        string s;
+        getline(cin, s);
+
+        printf("Agent#%d is at (%d, %d)\n", i + 1, a, b);
+    }
+}
