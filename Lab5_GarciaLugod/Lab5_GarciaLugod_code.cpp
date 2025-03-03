cea7d8e36c3b4b9286433da59238aed4b712332f
Waleed Lugod <lugodwaleed@gmail.com>
2025-03-03 08:08:25 +0800

    oath or smth

--- Lab5_GarciaLugod/Lab5_GarciaLugod_code.cpp
+++ Lab5_GarciaLugod/Lab5_GarciaLugod_code.cpp
@@ -1,3 +1,7 @@
+// I hereby attest to the truth of the following facts:
+// I have not discussed the c++ code in my program with anyone other than my instructor or the teaching assistants assigned to this course.
+// I have not used c++ code obtained from another student, or any other unauthorized source, whether modified or unmodified.
+// If any c++ code or documentation used in my program was obtained from another source, it has been clearly noted with citations in the comments of my program.
 #include <iostream>
 #include <cstdlib>
 using namespace std;
