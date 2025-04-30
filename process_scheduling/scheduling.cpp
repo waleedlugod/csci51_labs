f8b1004bace15bd30f40f3ce63c05d4a012a08f1
Waleed Lugod <lugodwaleed@gmail.com>
2025-05-01 00:26:44 +0800

    sort processes based on arrival time

--- process_scheduling/scheduling.cpp
+++ process_scheduling/scheduling.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <utility>
 using namespace std;
 
 void FCFS();
@@ -8,6 +9,8 @@ void SRTF();
 void P();
 void RR();
 
+void count_sort(int array[][3]);
+
 string line;
 ifstream input("input.txt");
 ofstream output("output.txt");
@@ -15,7 +18,6 @@ int test;
 int process_cnt;
 int q;
 int arrival, burst, priority;
-string process[3];
 int time_elapsed = 0;
 int cpu_time = 0;
 
@@ -41,24 +43,32 @@ int main()
             q = atoi(meta[1].c_str());
         }
 
+        string process_inputs[3];
+        int processes[process_cnt][3];
         for (int p = 1; p <= process_cnt; p++)
         {
-            input >> process[0] >> process[1] >> process[2];
-            arrival = atoi(process[0].c_str());
-            burst = atoi(process[1].c_str());
-            priority = atoi(process[2].c_str());
+            input >> process_inputs[0] >> process_inputs[1] >> process_inputs[2];
+            arrival = atoi(process_inputs[0].c_str());
+            burst = atoi(process_inputs[1].c_str());
+            priority = atoi(process_inputs[2].c_str());
+            processes[p - 1][0] = arrival;
+            processes[p - 1][1] = burst;
+            processes[p - 1][2] = priority;
         }
 
-        if (algo == "FCFS")
-            FCFS();
-        else if (algo == "SJF")
-            SJF();
-        else if (algo == "SRTF")
-            SRTF();
-        else if (algo == "P")
-            P();
-        else if (algo == "RR")
-            RR();
+        // sort arrival time
+        count_sort(processes);
+
+        // if (algo == "FCFS")
+        //     FCFS();
+        // else if (algo == "SJF")
+        //     SJF();
+        // else if (algo == "SRTF")
+        //     SRTF();
+        // else if (algo == "P")
+        //     P();
+        // else if (algo == "RR")
+        //     RR();
     }
 
     input.close();
@@ -66,53 +76,58 @@ int main()
     return 0;
 }
 
-void FCFS()
-{
-    for (int p = 1; p <= process_cnt; p++)
-    {
-        input >> process[0] >> process[1] >> process[2];
-        arrival = atoi(process[0].c_str());
-        burst = atoi(process[1].c_str());
-        priority = atoi(process[2].c_str());
-    }
+void FCFS() {
 };
-void SJF()
-{
-    for (int p = 1; p <= process_cnt; p++)
-    {
-        input >> process[0] >> process[1] >> process[2];
-        arrival = atoi(process[0].c_str());
-        burst = atoi(process[1].c_str());
-        priority = atoi(process[2].c_str());
-    }
+void SJF() {
 };
-void SRTF()
-{
-    for (int p = 1; p <= process_cnt; p++)
-    {
-        input >> process[0] >> process[1] >> process[2];
-        arrival = atoi(process[0].c_str());
-        burst = atoi(process[1].c_str());
-        priority = atoi(process[2].c_str());
-    }
+void SRTF() {
 };
-void P()
-{
-    for (int p = 1; p <= process_cnt; p++)
-    {
-        input >> process[0] >> process[1] >> process[2];
-        arrival = atoi(process[0].c_str());
-        burst = atoi(process[1].c_str());
-        priority = atoi(process[2].c_str());
-    }
+void P() {
 };
-void RR()
-{
-    for (int p = 1; p <= process_cnt; p++)
-    {
-        input >> process[0] >> process[1] >> process[2];
-        arrival = atoi(process[0].c_str());
-        burst = atoi(process[1].c_str());
-        priority = atoi(process[2].c_str());
-    }
+void RR() {
 };
+
+// https://www.geeksforgeeks.org/counting-sort/
+void count_sort(int inputArray[][3])
+{
+    int N = process_cnt;
+
+    // Finding the maximum element of array inputArray[].
+    int M = 0;
+
+    for (int i = 0; i < N; i++)
+        M = max(M, inputArray[i][0]);
+
+    // Initializing countArray[] with 0
+    int countArray[M + 1] = {0};
+
+    // Mapping each element of inputArray[] as an index
+    // of countArray[] array
+    for (int i = 0; i < N; i++)
+        countArray[inputArray[i][0]]++;
+
+    // Calculating prefix sum at every index
+    // of array countArray[]
+    for (int i = 1; i <= M; i++)
+        countArray[i] += countArray[i - 1];
+
+    // Creating outputArray[] from countArray[] array
+    int outputArray[N][3];
+
+    for (int i = N - 1; i >= 0; i--)
+    {
+        outputArray[countArray[inputArray[i][0]] - 1][0] = inputArray[i][0];
+        outputArray[countArray[inputArray[i][0]] - 1][1] = inputArray[i][1];
+        outputArray[countArray[inputArray[i][0]] - 1][2] = inputArray[i][2];
+
+        countArray[inputArray[i][0]]--;
+    }
+
+    // Copy outputArray to inputArray
+    for (int i = N - 1; i >= 0; i--)
+    {
+        inputArray[i][0] = outputArray[i][0];
+        inputArray[i][1] = outputArray[i][1];
+        inputArray[i][2] = outputArray[i][2];
+    }
+}
