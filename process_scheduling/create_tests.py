b3d46b81a2b24d16cf189e1494ae150bad5ff770
Waleed Lugod <lugodwaleed@gmail.com>
2025-04-30 21:01:02 +0800

    fix create tests

--- process_scheduling/create_tests.py
+++ process_scheduling/create_tests.py
@@ -4,12 +4,20 @@ f = open("input.txt", "w")
 test_cases = random.randint(5, 50)
 f.write(f"{test_cases}\n")
 for case in range(test_cases):
-    no_processes = random.randint(10, 20)
-    processes = [str(random.choice(range(50))) for _ in range(no_processes)]
+    no_processes = random.randint(1, 20)
     algorithm = random.choice(["FCFS", "SJF", "SRTF", "P", "RR"])
-    q = random.randint(1,5)
+    q = random.randint(1, 5)
+    meta = f"{no_processes} {algorithm}"
     if algorithm == "RR":
-        f.write(f"{no_processes} {algorithm} {q}\n")
-    else:
-        f.write(f"{no_processes} {algorithm}\n")
-    f.write(f"{' '.join(processes)}\n")
+        meta += f" {q}"
+    f.write(f"{meta}\n")
+    for process in range(no_processes):
+        arrival = random.randint(0, 20)
+        burst = random.randint(0, 20)
+        priority = random.randint(-20, 20)
+        f.write(f"{arrival} {burst} {priority}\n")
+    # if algorithm == "RR":
+    #     f.write(f"{no_processes} {algorithm} {q}\n")
+    # else:
+    #     f.write(f"{no_processes} {algorithm}\n")
+    # f.write(f"{' '.join(processes)}\n")
